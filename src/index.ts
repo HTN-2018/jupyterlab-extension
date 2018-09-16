aac1f6bc13ebb4993731164b79df17974f75aa0d
cdsupina <cdsupina@gmail.com>
2018-09-16 00:03:52 +0000

    add launcher icon and widget

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,84 @@ import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
-import '../style/index.css';
+import {
+  ILauncher
+} from '@jupyterlab/launcher';
 
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import '../style/index.css';
 
 /**
  * Initialization data for the htn-2018 extension.
  */
-const extension: JupyterLabPlugin<void> = {
+
+
+const plugin: JupyterLabPlugin<void> = {
   id: 'htn-2018',
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension htn-2018 is activated!');
-  }
+  requires: [ILauncher],
+  activate
 };
 
-export default extension;
+class KnocknockWidget extends Widget {
+
+  readonly img: HTMLImageElement;
+
+  constructor() {
+    super();
+
+    this.id = 'knocknock';
+    this.title.label = 'Knocknock';
+    this.title.closable = true;
+
+    this.img = document.createElement('img');
+    this.img.src = 'https://i.imgur.com/wXVZMRD.png';
+    this.node.appendChild(this.img)
+  }
+}
+
+function activate(app: JupyterLab, launcher: ILauncher){
+
+  let widget: KnocknockWidget;
+  const { commands } = app;
+
+  commands.addCommand('knocknock:open', {
+    label: 'Knocknock',
+    iconClass: 'jp-MaterialIcon knocknock_icon',
+    caption: 'Open Knocknock helper',
+    execute: () => {
+      //let cwd = browserFactory.defaultBrowser.model.path;
+      //return createNewDIO(cwd);
+      if (!widget){
+        //Create a new widget if one does not exist
+        widget = new KnocknockWidget();
+        widget.update();
+      }
+      //if(!tracker.has(widget)){
+      //  tracker.add(widget);
+      //}
+      if(!widget.isAttached){
+        app.shell.addToMainArea(widget);
+      }else{
+        widget.update();
+      }
+
+      //Activate the widget
+      app.shell.activateById(widget.id);
+    }
+});
+
+  if (launcher) {
+    launcher.add({
+      command: 'knocknock:open',
+      rank: 2,
+      category: 'Other'
+    });
+}
+  
+}
+
+export default plugin;
